9dfea9f2546af7b1c3719fc9961fa4e701d0ca01
bailamos <cieciumaly@gmail.com>
2021-02-06 15:04:42 +0100

    Working SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include <stdio.h>
-#include "./headers/example.hpp"
+#include "SDL.h" 
 
-int main() {
-    printf("Hello world!");
-    printf("Test: %i \n", add(1,1));
-    return 0;
+int main(int argc, char *argv[])
+{
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawLine(renderer, 0,0,50,50);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+  return 0;
 }
